c968144b6e17022d73fce7455c44b256e4bac65b
Eduardo.henrii <eduparaujo@outlook.com>
2023-01-26 11:52:03 -0300

    atualização utils

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,8 +1,7 @@
-import { TbBrandNextjs } from "react-icons/tb";
+
 import { SiPostgresql, SiTypescript } from "react-icons/si";
-import { FaGit, FaPython } from "react-icons/fa";
+import { FaGit } from "react-icons/fa";
 import {
-  FaDocker,
   FaHtml5,
   FaCss3Alt,
   FaJs,
